9bf984733b095cb817c19cf414cdf8ce8eab602a
Siba4442 <sibaprasadsahu4442@gmail.com>
2024-05-21 11:01:36 +0530

    Adding first Question of String Basic
    Added both .txt and .java files. Question name is Start coding

---
+++ 01_Java_Basic/Q1_Start_Coding.java
@@ -0,0 +1,26 @@
+class Geeks {
+
+    // Function to print hello
+    void printHello() {
+
+        System.out.println("Hello World");
+
+    }
+}
+
+// Driver code 
+
+public class Q1_Start_Coding {
+    public static void main (String [] args) {
+
+        // Creating an object of class Geeks 
+        Geeks g = new Geeks() ;
+
+        // Calling printHello() function of class Geeks
+        g.printHello();
+
+    }
+
+}
+
+// Driver code ends
